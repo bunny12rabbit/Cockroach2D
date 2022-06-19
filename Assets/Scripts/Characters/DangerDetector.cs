c1e4abdfbca67abacf7762b990b277ebc4e0cb5d
Andrey Romanenko <a.romanenko@gamythic.com>
2022-06-19 15:31:00 +0300

    DangerDetector implementation of IDangerDetector with IsWithinDangerArea(Vector3 position) method to check whether given coordinates is in danger range; also visual presentation of danger area bound to mouse cursor position;

---
+++ Assets/Scripts/Characters/DangerDetector.cs
@@ -0,0 +1,66 @@
+﻿using Core;
+using NaughtyAttributes;
+using UniRx;
+using UnityEngine;
+
+namespace Characters
+{
+    public class DangerDetector : InitializableMonoBehaviour<DangerDetector.Params>, IDangerDetector
+    {
+        public readonly struct Params
+        {
+            public readonly Camera Camera;
+
+            public Params(Camera camera)
+            {
+                Camera = camera;
+            }
+        }
+
+        [SerializeField, Required]
+        private SpriteRenderer _dangerousAreaImage;
+
+
+        [SerializeField]
+        private float _dangerRadius = 1.2f;
+
+        private Camera Camera => InputParams.Camera;
+
+        private Vector3 MouseWorldPosition
+        {
+            get
+            {
+                var position = Camera.ScreenToWorldPoint(Input.mousePosition);
+                position.z = 0;
+
+                return position;
+            }
+        }
+
+        private void OnValidate()
+        {
+            TrySetVisualSize();
+        }
+
+        private void TrySetVisualSize()
+        {
+            if (_dangerousAreaImage == null)
+                return;
+
+            transform.localScale = Vector2.one * _dangerRadius;
+        }
+
+        protected override void Init()
+        {
+            TrySetVisualSize();
+            Observable.EveryUpdate().Subscribe(Tick).AddTo(Disposables);
+        }
+
+        private void Tick(long _)
+        {
+            transform.position = MouseWorldPosition;
+        }
+
+        public bool IsWithinDangerArea(Vector3 position) => Vector3.Distance(MouseWorldPosition, position) <= _dangerRadius;
+    }
+}
