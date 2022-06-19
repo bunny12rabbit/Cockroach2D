6898e5b4111722dee4f45972531a256f165e0e22
Andrey Romanenko <a.romanenko@gamythic.com>
2022-06-19 15:28:31 +0300

    CharacterData implementation of ICharacterData with base params such as NormalSpeed, RunAwayAccelerationDelta and AccelerationCurve

---
+++ Assets/Scripts/Characters/CharacterData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters
+{
+    [Serializable]
+    public struct CharacterData : ICharacterData
+    {
+        public static CharacterData Default = new(5, 0.05f);
+
+        [SerializeField]
+        private float _normalSpeed;
+        public float NormalSpeed => _normalSpeed;
+
+        [SerializeField]
+        private float _runAwayAccelerationDelta;
+        public float RunAwayAccelerationDelta => _runAwayAccelerationDelta;
+
+        [SerializeField]
+        private AnimationCurve _accelerationCurve;
+        public AnimationCurve AccelerationCurve => _accelerationCurve;
+
+
+        public CharacterData(float normalSpeed, float runAwayAccelerationDelta) : this()
+        {
+            _normalSpeed = normalSpeed;
+            _runAwayAccelerationDelta = runAwayAccelerationDelta;
+            _accelerationCurve = AnimationCurve.EaseInOut(0f, 0f, 2f, 1f);
+        }
+    }
+}
