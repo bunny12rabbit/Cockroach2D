6e687cc28c38234738d8e27562eae86f9c1a79fc
Andrey Romanenko <a.romanenko@gamythic.com>
2022-06-19 15:33:24 +0300

    GameDesignSettings.cs and GameDesignSettings.asset ScriptableObject with configuration of game. So far there's CharacterData setup.

---
+++ Assets/Scripts/Common/Settings/GameDesignSettings.cs
@@ -0,0 +1,18 @@
+﻿using Characters;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Common.Settings
+{
+    [CreateAssetMenu(fileName = nameof(GameDesignSettings), menuName = "Settings/Game Design Settings")]
+    public class GameDesignSettings : ScriptableObject
+    {
+        public const string AssetPath = "Assets/GameDesignSettings/GameDesignSettings.asset";
+
+        private const string EnemySettingsLabel = "Enemy Settings Label";
+
+        [SerializeField, BoxGroup(EnemySettingsLabel)]
+        private CharacterData _characterData = CharacterData.Default;
+        public CharacterData CharacterData => _characterData;
+    }
+}
